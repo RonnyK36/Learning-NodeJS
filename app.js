e41565a1189b1622de05b7fb37fba04806b77d49
RonnyK36 <ronnykelvyne3@gmail.com>
2022-11-08 10:52:04 +0300

    merging

--- app.js
+++ app.js
@@ -1,17 +1,4 @@
-// Modules = Encapsulates coed (shares minimumly)
-// Every  file is a module.
-const {
-    daughter,
-    mother,
-    title,
-    location
-} = require('./module-names')
-const { sayHi } = require('./utils')
-const data = require('./alternative-export')
+const os = require('os')
 
-// console.log(names);
-
-sayHi('Kevin')
-sayHi(location)
-sayHi(title)
-console.log(data);
+const user = os.userInfo()
+console.log(user);
